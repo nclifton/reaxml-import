c848ded1748ce76c562d3a94a7ae6cf979acfe05
Neil Clifton <Clifton IT Foundries Pty Ltd>
2014-09-12 00:10:23 +1000

    Signed-off-by: Neil Clifton <Clifton IT Foundries Pty Ltd>

--- REAXMLImport/admin/views/display/html.php
+++ REAXMLImport/admin/views/display/html.php
@@ -7,7 +7,7 @@ defined ( '_JEXEC' ) or die ( 'Restricted access' );
  * @license GNU General Public License version 2 or later; see LICENSE.txt
  *         
  */
-defined ( '_JEXEC' ) or die ( 'Restricted access' );
+
 class ReaXmlImportViewsDisplayHtml extends JViewHtml {
 	function render() {
 		// retrieve task list from model
