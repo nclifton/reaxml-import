5378d60ce58e27a3e531f99f3e94f0376c940512
Neil Clifton <Clifton IT Foundries Pty Ltd>
2014-09-12 14:11:30 +1000

    Signed-off-by: Neil Clifton <Clifton IT Foundries Pty Ltd>

--- REAXMLImport/admin/models/logs.php
+++ REAXMLImport/admin/models/logs.php
@@ -2,11 +2,13 @@
 defined ( '_JEXEC' ) or die ( 'Restricted access' );
 
 /**
- *
- * @copyright Copyright (C) 2014 Clifton IT Foundries Pty Ltd All rights reserved.
- * @license GNU General Public License version 2 or later; see LICENSE.txt
- *         
- */
+ * @package Component REAXML Import for EZ Realty for Joomla! 3.3
+ * @version 0.43.119: logs.php 2014-09-12T13:54:30.355
+ * @author Neil Clifton
+ * @link http://cliftonwebfoundry.com.au
+ * @copyright Copyright (c) 2014 Clifton IT Foundries Pty Ltd. All rights Reserved
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ **/
 class ReaXmlImportModelsLogs extends JModelBase {
 	public function getLogFiles() {
 		$params = JComponentHelper::getParams ( 'com_reaxmlimport' );
