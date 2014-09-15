7ca1f1386a79ec3e382e32e7f782afcc1114b9c1
Neil Clifton <Clifton IT Foundries Pty Ltd>
2014-09-15 16:02:46 +1000

    Signed-off-by: Neil Clifton <Clifton IT Foundries Pty Ltd>

--- REAXMLImport/admin/helpers/admin.php
+++ REAXMLImport/admin/helpers/admin.php
@@ -2,13 +2,21 @@
 defined ( '_JEXEC' ) or die ( 'Restricted access' );
 
 /**
- * @package Component REAXML Import for EZ Realty for Joomla! 3.3
- * @version 0.43.119: admin.php 2014-09-12T13:54:30.355
- * @author Neil Clifton
- * @link http://cliftonwebfoundry.com.au
- * @copyright Copyright (c) 2014 Clifton IT Foundries Pty Ltd. All rights Reserved
- * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package Joomla.Administrator
+ * @subpackage com_reaxmlimport
+ *            
+ * @copyright Copyright (C) 2014 Clifton IT Foundries Pty Ltd All rights reserved.
+ * @license GNU General Public License version 2 or later; see LICENSE.txt
  **/
+
+class ReaXmlImportHelpersAdmin {
+	public static $extension = 'com_reaxmlimport';
+	
+	/**
+	 *
+	 * @return JObject
+	 */
 	public static function getActions() {
 		$user = JFactory::getUser ();
 		$result = new JObject ();
