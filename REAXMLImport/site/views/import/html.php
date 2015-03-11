48d4dcd750691bd559480e90cc2ef18c34076069
Neil Clifton <Clifton IT Foundries Pty Ltd>
2015-03-12 02:22:55 +1100

    Signed-off-by: Neil Clifton <Clifton IT Foundries Pty Ltd>

--- REAXMLImport/site/views/import/html.php
+++ REAXMLImport/site/views/import/html.php
@@ -3,7 +3,7 @@ defined ( '_JEXEC' ) or die ( 'Restricted access' );
 
 /**
  * @package Component REAXML Import for Joomla! 3.3
- * @version 0.43.123: html.php 2015-03-11T22:17:34.616
+ * @version 0.43.127: html.php 2015-03-12T02:22:18.590
  * @author Clifton IT Foundries Pty Ltd
  * @link http://cliftonwebfoundry.com.au
  * @copyright Copyright (c) 2014 Clifton IT Foundries Pty Ltd. All rights Reserved
