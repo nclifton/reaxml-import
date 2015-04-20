2243050f7db60c1ad65231d00533473bf42f181c
Neil Clifton <Clifton IT Foundries Pty Ltd>
2015-04-21 01:04:31 +1000

    Signed-off-by: Neil Clifton <Clifton IT Foundries Pty Ltd>

--- ReaXmlLibrary/lib/reaxml/ezr/col/declat.php
+++ ReaXmlLibrary/lib/reaxml/ezr/col/declat.php
@@ -2,14 +2,14 @@
 defined ( '_JEXEC' ) or die ( 'Restricted access' );
 
 /**
- * 
+ *
  * @package Library REAXML Library for Joomla! 3.4
- * @version 1.2.5: declat.php 2015-04-07T14:41:26.244
+ * @version 1.3.1: declat.php 2015-04-21T01:03:15.478
  * @author Clifton IT Foundries Pty Ltd
  * @link http://cliftonwebfoundry.com.au
  * @copyright Copyright (c) 2014 Clifton IT Foundries Pty Ltd. All rights Reserved
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- * 
+ *
  **/ 
 class ReaxmlEzrColDeclat extends \ReaxmlEzrColumn {
 	const XPATH_ADDRESS_LATITUDE = '//address/latitude';
