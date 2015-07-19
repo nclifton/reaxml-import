9f4c4a79b3dfa87225e6ebf38a3075176139a8f5
nclifton <neil.clifton@cliftonwebfoundry.com.au>
2015-07-19 16:52:29 +1000

    -m well?

--- REAXMLImport/tests/unite/log/unite.log.php
+++ REAXMLImport/tests/unite/log/unite.log.php
@@ -1,44 +1,37 @@
-<?php die(); // Protect from prying eyes! ?>20150617|17:53:22|Starting Akeeba UNITE v.1.4.2 (2015-01-26)
-20150617|17:53:22|Scanning /Users/nclifton/Documents/marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/inbox for XML files...
-20150617|17:53:22|Parsing /Users/nclifton/Documents/marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/inbox/site-reaxml.dev.xml
-20150617|17:53:22|Found 1 job(s) to run
-20150617|17:53:22|### Starting job #1 ###
-20150617|17:53:22|Running validator
-20150617|17:53:22|Running remote
-20150617|17:53:22|Running s3
-20150617|17:53:22|Running stealth
-20150617|17:53:22|Writing a stealth .htaccess (direct access)
-20150617|17:53:22|Running extract
-20150617|17:53:22|Extractor tick
-20150617|17:53:28|Extractor tick
-20150617|17:53:28|Running dbrestore
-20150617|17:53:28|Processing database site
-20150617|17:53:28|Trying to retrieve installation/sql/site.sql using direct access
-20150617|17:53:28|Importing site.sql to database...
-20150617|17:53:31|Trying to retrieve installation/sql/site.s01 using direct access
-20150617|17:53:31|Running configupdate
-20150617|17:53:31|Retrieving configuration.php...
-20150617|17:53:31|Trying to retrieve configuration.php using direct access
-20150617|17:53:31|Updating site's Global Configuration
-20150617|17:53:31|Saving configuration.php...
-20150617|17:53:31|Trying to retrieve libraries/bitfolge/feedcreator.php using direct access
-20150617|17:53:31|Trying to retrieve libraries/joomla/form/form.php using direct access
-20150617|17:53:31|Trying to retrieve libraries/cms/captcha/captcha.php using direct access
-20150617|17:53:31|Trying to retrieve libraries/cms/ucm/base.php using direct access
-20150617|17:53:31|Modifying Super Administrator user
-20150617|17:53:31|The username admin is already taken! Skipping...
-20150617|17:53:31|Running customsql
-20150617|17:53:31|Running cleanup
-20150617|17:53:31|Cleaning up
-20150617|17:53:31|Recursively removing installation directory (direct)
-20150617|17:53:31|Removing stealth .htaccess and restoring the original
-20150617|17:53:31|Running customfiles
-20150617|17:53:31|Running emailnotify
-20150617|17:53:31|Processing email notifications
-20150617|17:53:31|### Finished job #1 ###
-20150617|17:53:31|Running postbanner
-20150617|17:53:31|===============================================================================
-20150617|17:53:31|UNITE finished its run cycle
-20150617|17:53:31|Total definitions found                : 1
-20150617|17:53:31|Total definitions executed successfuly : 1
-20150617|17:53:31|Total definitions failed to run        : 0
+<?php die(); // Protect from prying eyes! ?>20150706|11:26:14|Starting Akeeba UNITE v.1.4.2 (2015-01-26)
+20150706|11:26:14|Scanning /Users/nclifton/Documents/marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/inbox for XML files...
+20150706|11:26:14|Parsing /Users/nclifton/Documents/marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/inbox/site-reaxml.dev.xml
+20150706|11:26:14|Found 1 job(s) to run
+20150706|11:26:14|### Starting job #1 ###
+20150706|11:26:14|Running validator
+20150706|11:26:14|Running remote
+20150706|11:26:14|Running s3
+20150706|11:26:14|Running stealth
+20150706|11:26:14|Writing a stealth .htaccess (direct access)
+20150706|11:26:14|Running extract
+20150706|11:26:14|Extractor tick
+20150706|11:26:20|Extractor tick
+20150706|11:26:20|Running dbrestore
+20150706|11:26:20|Processing database site
+20150706|11:26:20|Trying to retrieve installation/sql/site.sql using direct access
+20150706|11:26:20|Importing site.sql to database...
+20150706|11:26:20|PHP WARNING on line 231 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
+20150706|11:26:20|mysql_query() expects parameter 2 to be resource, boolean given
+20150706|11:26:20|PHP WARNING on line 235 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
+20150706|11:26:20|mysql_errno() expects parameter 1 to be resource, boolean given
+20150706|11:26:20|PHP WARNING on line 236 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
+20150706|11:26:20|mysql_error() expects parameter 1 to be resource, boolean given
+20150706|11:26:20|PHP WARNING on line 231 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
+20150706|11:26:20|mysql_query() expects parameter 2 to be resource, boolean given
+20150706|11:26:20|PHP WARNING on line 235 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
+20150706|11:26:20|mysql_errno() expects parameter 1 to be resource, boolean given
+20150706|11:26:20|PHP WARNING on line 236 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
+20150706|11:26:20|mysql_error() expects parameter 1 to be resource, boolean given
+20150706|11:26:20|SQL error dropping #__ak_profiles:  SQL=DROP TABLE IF EXISTS `jos_ak_profiles`;
+20150706|11:26:20|### Finished job #1 ###
+20150706|11:26:20|Running postbanner
+20150706|11:26:20|===============================================================================
+20150706|11:26:20|UNITE finished its run cycle
+20150706|11:26:20|Total definitions found                : 1
+20150706|11:26:20|Total definitions executed successfuly : 0
+20150706|11:26:20|Total definitions failed to run        : 1
