a959164fecc31f7c3ecb30ed14be23236307718e
nclifton <neil.clifton@cliftonwebfoundry.com.au>
2016-03-06 14:07:45 +1100

    commit

--- REAXMLImport/tests/unite/log/site-reaxml.dev.log.php
+++ REAXMLImport/tests/unite/log/site-reaxml.dev.log.php
@@ -1,24 +1,54 @@
-<?php die(); // Protect from prying eyes! ?>20150706|11:26:14|Running remote
-20150706|11:26:14|Running s3
-20150706|11:26:14|Running stealth
-20150706|11:26:14|Writing a stealth .htaccess (direct access)
-20150706|11:26:14|Running extract
-20150706|11:26:14|Extractor tick
-20150706|11:26:20|Extractor tick
-20150706|11:26:20|Running dbrestore
-20150706|11:26:20|Processing database site
-20150706|11:26:20|Trying to retrieve installation/sql/site.sql using direct access
-20150706|11:26:20|Importing site.sql to database...
-20150706|11:26:20|PHP WARNING on line 231 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
-20150706|11:26:20|mysql_query() expects parameter 2 to be resource, boolean given
-20150706|11:26:20|PHP WARNING on line 235 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
-20150706|11:26:20|mysql_errno() expects parameter 1 to be resource, boolean given
-20150706|11:26:20|PHP WARNING on line 236 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
-20150706|11:26:20|mysql_error() expects parameter 1 to be resource, boolean given
-20150706|11:26:20|PHP WARNING on line 231 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
-20150706|11:26:20|mysql_query() expects parameter 2 to be resource, boolean given
-20150706|11:26:20|PHP WARNING on line 235 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
-20150706|11:26:20|mysql_errno() expects parameter 1 to be resource, boolean given
-20150706|11:26:20|PHP WARNING on line 236 in file /Users/nclifton/Documents/Marseclipse/workspace/reaxml-import/REAXMLImport/tests/unite/engine/driver/mysql.php:
-20150706|11:26:20|mysql_error() expects parameter 1 to be resource, boolean given
-20150706|11:26:20|SQL error dropping #__ak_profiles:  SQL=DROP TABLE IF EXISTS `jos_ak_profiles`;
+<?php die(); // Protect from prying eyes! ?>20150721|11:30:45|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:30:45|Only variable references should be returned by reference
+20150721|11:30:45|Running remote
+20150721|11:30:45|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:30:45|Only variable references should be returned by reference
+20150721|11:30:45|Running s3
+20150721|11:30:45|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:30:45|Only variable references should be returned by reference
+20150721|11:30:45|Running stealth
+20150721|11:30:45|Writing a stealth .htaccess (direct access)
+20150721|11:30:45|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:30:45|Only variable references should be returned by reference
+20150721|11:30:45|Running extract
+20150721|11:30:45|Extractor tick
+20150721|11:30:58|Extractor tick
+20150721|11:31:08|Extractor tick
+20150721|11:31:08|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:31:08|Only variable references should be returned by reference
+20150721|11:31:08|Running dbrestore
+20150721|11:31:08|Processing database site
+20150721|11:31:08|Trying to retrieve installation/sql/site.sql using direct access
+20150721|11:31:08|Importing site.sql to database...
+20150721|11:31:13|Trying to retrieve installation/sql/site.s01 using direct access
+20150721|11:31:13|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:31:13|Only variable references should be returned by reference
+20150721|11:31:13|Running configupdate
+20150721|11:31:13|Retrieving configuration.php...
+20150721|11:31:13|Trying to retrieve configuration.php using direct access
+20150721|11:31:13|Updating site's Global Configuration
+20150721|11:31:13|Saving configuration.php...
+20150721|11:31:13|Trying to retrieve libraries/bitfolge/feedcreator.php using direct access
+20150721|11:31:13|Trying to retrieve libraries/joomla/form/form.php using direct access
+20150721|11:31:13|Trying to retrieve libraries/cms/captcha/captcha.php using direct access
+20150721|11:31:13|Trying to retrieve libraries/cms/ucm/base.php using direct access
+20150721|11:31:13|Modifying Super Administrator user
+20150721|11:31:13|The username admin is already taken! Skipping...
+20150721|11:31:13|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:31:13|Only variable references should be returned by reference
+20150721|11:31:13|Running customsql
+20150721|11:31:13|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:31:13|Only variable references should be returned by reference
+20150721|11:31:13|Running cleanup
+20150721|11:31:13|Cleaning up
+20150721|11:31:13|Recursively removing installation directory (direct)
+20150721|11:31:13|Removing stealth .htaccess and restoring the original
+20150721|11:31:13|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:31:13|Only variable references should be returned by reference
+20150721|11:31:13|Running customfiles
+20150721|11:31:13|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:31:13|Only variable references should be returned by reference
+20150721|11:31:13|Running emailnotify
+20150721|11:31:13|Processing email notifications
+20150721|11:31:13|PHP NOTICE on line 75 in file /Users/nclifton/git/reaxml-import/REAXMLImport/tests/unite/engine/factory.php:
+20150721|11:31:13|Only variable references should be returned by reference
